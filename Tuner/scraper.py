399eb5a1ba0b75fef8be51de7c3790a74c764b96
Durucz Béla <bela.durucz@borealisengineering.hu>
2022-08-18 20:49:35 +0200

    Movie links
    Get each top rated movie's link

--- Tuner/scraper.py
+++ Tuner/scraper.py
@@ -3,8 +3,15 @@ IMDB scraper
 """
 
 import urllib.request, urllib.error, urllib.parse
+from bs4 import BeautifulSoup
 
 
 url = "https://www.imdb.com/chart/top/"
 response = urllib.request.urlopen(url)
 imdb_page = response.read().decode('UTF-8')
+movies = BeautifulSoup(imdb_page, 'html.parser')
+
+movies_table = movies.find(class_="lister-list")
+movie_links = []
+for tag in movies_table.find_all("tr"):
+    movie_links.append(tag.find("a")["href"])
